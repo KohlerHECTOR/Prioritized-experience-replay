167321b5538789d7c1ea9fdcb4374bf65fd2ba91
KohlerHECTOR <>
2022-01-08 14:45:29 +0100

    fixed seed problem for experiments

--- dynaq.py
+++ dynaq.py
@@ -21,10 +21,9 @@ class DynaQAgent():
         self.states = np.arange(self.mdp.nb_states)
         self.Model = {} # empty dictionary a model will look like this: Model[(S,A)] = (R, S')
         self.visited_states = {} # visited states during real experiments vis_states[S] = [False, False, True, False]
-        # self.M = np.zeros((self.mdp.nb_states, self.mdp.nb_states)) # successor representation matrix.
-        # self.Gain = np.zeros((self.mdp.nb_states, self.mdp.action_space.size)) # mGain matrix
 
-    def evaluate(self, eps):
+
+    def evaluate(self, eps, seed):
         """
         simple function to evaluate a policy from a Q table
         """
@@ -33,15 +32,14 @@ class DynaQAgent():
         s = self.mdp.current_state
         done = self.mdp.done()
 
+        np.random.seed(seed)
         steps_to_exit = 0
         while not done:
-            # Draw an action using a softmax policy
+
 
             a = egreedy(self.Q, s, eps)
-            # prob_a = softmax(self.Q, s, tau)
-            # a = np.random.choice(self.actions, p = prob_a)
             s, r, done, _  = self.mdp.step(a)
-            # s, r, done, _  = self.mdp.step(np.argmax(self.Q[s,:]))
+
             steps_to_exit += 1
 
         return steps_to_exit
@@ -65,24 +63,6 @@ class DynaQAgent():
         """
         self.Model[(s, a)] = (r, s_next)
 
-    # def update_SR_matrix(self, s, s_next):
-    #     """
-    #     Updates successor representation matrix using TD difference error.
-    #     """
-    #     one_hot = np.zeros(self.mdp.nb_states)
-    #     one_hot[s] = 1
-    #     delta = one_hot + self.mdp.gamma * self.M[s_next, :] - self.M[s, :] # delta is vector
-        # self.M[s, :] += self.alpha * delta
-
-    # def update_Gain(self, q_old, q_new, s, a, tau):
-    #     """
-    #     Update gain matrix using difference in estimated return due to performing a belmman backup for the state action pair.
-    #     """
-    #     policy_old = softmax(q_old, s, tau)
-    #     policy_new = softmax(q_new, s, tau)
-    #     self.Gain[s, a] = np.sum(q_new[s, :] * policy_new) - np.sum(q_new[s, :] * policy_old)
-
-
     def planning(self, eps):
         """
         Performs a planning step (to be repeated n times)
@@ -102,22 +82,10 @@ class DynaQAgent():
 
         if s in self.mdp.terminal_states:
             self.Q[s, a] = self.alpha * r
-            # q_new = self.Q.copy()
-
-            # self.update_Gain(q_old, q_new, s, a, tau)
-            # update SR matrix
-            # one_hot = np.zeros(self.mdp.nb_states)
-            # one_hot[s] = 1
-            # self.M[s, :] += self.alpha * one_hot
 
         else:
             # Backup
             self.backup(s, a, r, s_next)
-            # q_new = self.Q.copy()
-            # update Gain
-            # self.update_Gain(q_old, q_new, s, a, tau)
-            # update successor representation matrix
-            # self.update_SR_matrix(s, s_next)
 
     def learn(self,
               eps: float, # epsilon parameter for eps-greedy pol
@@ -140,12 +108,11 @@ class DynaQAgent():
             self.mdp.new_render("Dyna-Q e-greedy")
 
         for _ in range(nb_episodes):
-            # Draw the first state of episode i using a uniform distribution over all the states
 
             s = self.mdp.reset()
-            self.mdp.current_state = 0 #Mattar's simulation always starts from same state.
+            self.mdp.current_state = 0 #Sutton&Barto's simulation always starts from same state.
             s = self.mdp.current_state
-            np.random.seed(seed)
+            # np.random.seed(seed)
             done = self.mdp.done()
             steps = 0
             while not done:
@@ -157,9 +124,6 @@ class DynaQAgent():
                     self.mdp.render(self.Q, self.Q.argmax(axis=1))
 
                 a = egreedy(self.Q, s, eps)
-                # Draw an action using a softmax policy
-                # prob_a = softmax(self.Q, s, tau)
-                # a = np.random.choice(self.actions, p = prob_a)
                 # Update executed acions in s
                 self.visited_states[s][a] = True
                 # Execute action a
@@ -168,29 +132,14 @@ class DynaQAgent():
 
                 if s in self.mdp.terminal_states:
                     self.Q[s, a] = self.alpha * r
-                    # q_new = self.Q.copy()
-                    # self.update_Gain(q_old, q_new, s, a, tau)
-                    # update SR matrix
-                    # one_hot = np.zeros(self.mdp.nb_states)
-                    # one_hot[s] = 1
-                    # self.M[s, :] += self.alpha * one_hot
-
 
                 else:
                     # Backup
-                    # print(s, s_next)
-
                     self.backup(s, a, r, s_next)
-                    # q_new = self.Q.copy()
-                    # update the Gain matrix
-                    # self.update_Gain(q_old, q_new, s, a, tau)
-                    # update successor representation matrix
-                    # self.update_SR_matrix(s, s_next)
 
 
 
                 # Update Model
-                # print(s, a, r, s_next)
                 self.update_model(s, a, r, s_next)
 
 
@@ -202,7 +151,7 @@ class DynaQAgent():
                 s = s_next
 
 
-            steps_to_exit = self.evaluate(eps)
+            steps_to_exit = self.evaluate(eps, seed)
 
             list_steps_episode.append(steps_to_exit)
 
--- sutton_Barto_fig_8_5.py
+++ sutton_Barto_fig_8_5.py
@@ -13,54 +13,50 @@ from dynaq import DynaQAgent
 # For visualization
 os.environ["VIDEO_FPS"] = "5"
 
-"""
-Just launch python3 main.py to reproduce fig 8.5 from Sutton and Barto's Intro to RL
-2nd edition, figure 8.5 .
-"""
 
 mdp = create_random_maze(9, 6, 0.13)
 
 # Sutton and Barto parameters (figure 8.5 from Intro to RL) with eps_greedy pol
 # alpha = 0.1, eps = 0.1, gamma = 0.95
 
-# Mattar paraemeters with softmax policy
 ALPHA = 0.1
 EPS = 0.1
-# BETA = 5 # inverse temperature
-# TAU = 1/BETA
 GAMMA = 0.95
-NB_EXP =  50
+NB_EXP =  30
 
 print("-------- Rat does 0 steps of planning ----------")
 to_plot_planning_0 = []
+
 rat = DynaQAgent(mdp, alpha = ALPHA, gamma = GAMMA)
+
 for i in range(NB_EXP):
     to_plot_planning_0.append(rat.learn(eps = EPS, nb_episodes = 50, render = False, n = 0, timeout = 20000, seed = i ))
     print("Experience number: {}".format(i + 1))
+
 to_plot_planning_0 = np.array(to_plot_planning_0).mean(axis = 0)
 
 print("-------- Rat does 5 steps of planning ----------")
 to_plot_planning_5 = []
+
 rat = DynaQAgent(mdp, alpha = ALPHA, gamma = GAMMA)
+
 for i in range(NB_EXP):
     to_plot_planning_5.append(rat.learn(eps = EPS, nb_episodes = 50, render = False, n = 5, timeout = 20000, seed = i))
     print("Experience number: {}".format(i + 1))
+
 to_plot_planning_5 = np.array(to_plot_planning_5).mean(axis = 0)
 
 print("-------- Rat does 50 steps of planning ----------")
 to_plot_planning_50 = []
+
 rat = DynaQAgent(mdp, alpha = ALPHA, gamma = GAMMA)
+
 for i in range(NB_EXP):
     to_plot_planning_50.append(rat.learn(eps = EPS, nb_episodes = 50, render = False, n = 50, timeout = 20000, seed = i))
     print("Experience number: {}".format(i + 1))
+
 to_plot_planning_50 = np.array(to_plot_planning_50).mean(axis = 0)
 
-
-# print(np.argmax(rat.M[0, :]))
-# plot_need(9, 6, rat.M)
-
-
-plt.clf()
 plt.plot(to_plot_planning_0[:], label = "0 planning steps")
 plt.plot(to_plot_planning_5[:], label = "5 planning steps")
 plt.plot(to_plot_planning_50[:], label = "50 planning steps")
@@ -69,7 +65,3 @@ plt.ylabel("steps to goal")
 plt.title("Evaluation of Sutton & Barto's Dyna-Q agent")
 plt.legend()
 plt.savefig("results/Sutton&Barto/figure_8_5.png")
-plt.clf()
-# rat.mdp.current_state = 0
-# rat.mdp.render(rat.Q, np.argmax(rat.Q, axis=1), title="Dyna-Q softmax")
-# plt.savefig("results/q_table_dyna_q_50_steps_planning_gain_need_softmax")
