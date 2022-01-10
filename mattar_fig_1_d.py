3cddb2e704b173afac445104040b07258c6a9180
KohlerHECTOR <>
2022-01-10 18:29:32 +0100

    changes

--- mattar_fig_1_d.py
+++ mattar_fig_1_d.py
@@ -9,7 +9,7 @@ from mazemdp import create_random_maze
 from replay_sim import Agent
 from utils import evaluate
 
-from arguments import get_args
+from arguments import get_args, get_args_string
 
 
 args = get_args()
@@ -23,13 +23,13 @@ def to_plot(data, label):
     plt.fill_between(np.arange(len(mean_data)), mean_data + std_data, mean_data - std_data, alpha = 0.4)
 
 
-mdp = create_random_maze(9, 6, 0.13)
+mdp = create_random_maze(9, 6, 0.2)
 
 # using e-greedy pol for all models
 args.action_policy = "greedy"
 args.epsilon = 0
 args.start_random = True
-# args.expand_further = False
+print(get_args_string(args))
 #### MATTAR MODEL ####
 print("WITH PRIORITIZED REPLAY")
 res_train_prio = []
@@ -37,15 +37,15 @@ res_list_Q = []
 for i in range(args.simulations):
     print("#### SIM NB: {}".format(i))
     rat = Agent(mdp, args)
-    data = rat.learn(args)
+    data = rat.learn(args, seed = i)
     res_train_prio.append(data["train"])
     res_list_Q.append(data["list_Q"])
 
-res_eval = []
-for list_Q in res_list_Q:
-    res_eval.append(evaluate(list_Q, mdp, args))
-
-to_plot(np.array(res_eval), label = "Prioritized replay")
+# res_eval = []
+# for list_Q in res_list_Q:
+#     res_eval.append(evaluate(list_Q, mdp, args))
+#
+# to_plot(np.array(res_eval), label = "Prioritized replay")
 #### MATTAR MODEL ####
 
 #### Dyna - Q ######
@@ -57,15 +57,15 @@ res_list_Q = []
 for i in range(args.simulations):
     print("#### SIM NB: {}".format(i))
     rat = Agent(mdp, args)
-    data = rat.learn(args)
+    data = rat.learn(args, seed = i)
     res_train_dyna.append(data["train"])
     res_list_Q.append(data["list_Q"])
 
-res_eval = []
-for list_Q in res_list_Q:
-    res_eval.append(evaluate(list_Q, mdp, args))
-
-to_plot(np.array(res_eval), label = "Dyna-Q")
+# res_eval = []
+# for list_Q in res_list_Q:
+#     res_eval.append(evaluate(list_Q, mdp, args))
+#
+# to_plot(np.array(res_eval), label = "Dyna-Q")
 #### Dyna - Q ######
 
 #### Q-learning######
@@ -78,30 +78,30 @@ res_list_Q = []
 for i in range(args.simulations):
     print("#### SIM NB: {}".format(i))
     rat = Agent(mdp, args)
-    data = rat.learn(args)
+    data = rat.learn(args, seed = i)
     res_train_nothing.append(data["train"])
     res_list_Q.append(data["list_Q"])
 
-res_eval = []
-for list_Q in res_list_Q:
-    res_eval.append(evaluate(list_Q, mdp, args))
-
-to_plot(np.array(res_eval), label = "Q-learning")
+# res_eval = []
+# for list_Q in res_list_Q:
+#     res_eval.append(evaluate(list_Q, mdp, args))
+#
+# to_plot(np.array(res_eval), label = "Q-learning")
 #### Q-learning ######
 
 
 
-
-# to_plot(np.array(res_train), "train")
-plt.xlabel("episode")
-plt.ylabel("steps until goal")
-# plt.title("Performances of Mattar's agent in training and evalutation")
-plt.legend()
-filename = "mattar_fig_1_d_eval"
-# for _, val in args._get_kwargs():
-#     filename += "_" + str(val)
-plt.savefig("results/Mattar/" + filename + ".png")
-plt.clf()
+#
+# # to_plot(np.array(res_train), "train")
+# plt.xlabel("episode")
+# plt.ylabel("steps until goal")
+# # plt.title("Performances of Mattar's agent in training and evalutation")
+# plt.legend()
+# filename = "mattar_fig_1_d_eval"
+# # for _, val in args._get_kwargs():
+# #     filename += "_" + str(val)
+# plt.savefig("results/Mattar/" + filename + ".png")
+# plt.clf()
 
 to_plot(np.array(res_train_prio), label = "Prioritized replay")
 to_plot(np.array(res_train_dyna), label = "Dyna-Q")
